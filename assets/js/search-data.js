07caa928f1931b83c24b7f88331817e49ec294fa
shjung13 <shjung13@cs.washington.edu>
2026-08-24 01:06:39 +0000

    Deploying to gh-pages from @ shjung13/shjung13.github.io@c9f3afbb979f4ec10a6cfc3e76244e7d2b96e097 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Last Update: 03/24/2026",
+          description: "Last Update: 08/23/2026",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
